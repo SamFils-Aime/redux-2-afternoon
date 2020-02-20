937bc42cd66962997fa4c429334161276a443a96
samFils-Aime <samfilsaime@hotmail.com>
2020-02-20 08:45:11 -0600

    aa

--- src/index.js
+++ src/index.js
@@ -2,9 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+import Store from './ducks/store';
+
+// WRAPPING THE APP COMPONENT IN T
 
 ReactDOM.render(
-  <App />
+  <Provider store={Store}>
+    <App />
+  </Provider>
   , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
